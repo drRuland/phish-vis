710b5e1943b5ce4b381f4a6f96b8c5285c4a4c73
drRuland <phdbgg.04@gmail.com>
2015-03-21 22:39:54 -0700

    refactored the code to add a songs_played field to document and add to mongodb easier

--- phishNet.py
+++ phishNet.py
@@ -1,166 +1,241 @@
 import urllib, json, sys, pprint, re, time
-from datetime import date
+import datetime
 from dateutil.rrule import rrule, DAILY
 from bs4 import BeautifulSoup as bs
 from pymongo import MongoClient
+import argparse
 
-## These are constant values that the 
-## API call will need every time
+##------------------------------------##
+## These are constant values that the ##
+##   API call will need every time    ##
+##------------------------------------##
 url = "https://api.phish.net/"
 endpoint = "api.js"
 format='json'
 apiver='2.0'
 
-## Get this from the file
-apikey = ""
-
-## this is the driver for the connection to the mongodb
+##-----------------------------------##
+## Connection information to MongoDB ##
+##-----------------------------------##
 client = MongoClient()
 db = client.phish
 pshows = db.phishshows
 
-def clean_set(setlist):
-	""" 
-	This function takes a setlist from phish.net and cleans it up 
-	by splitting it into its songs and keeping segue information.
+##---------------------------------------------##
+## Date for comparison to see if we should get ##
+##   show information and add to database      ##
+##---------------------------------------------##
+todays_date = datetime.datetime.today().date()
+this_year = todays_date.year
 
-	ex. 
-	input: "Set 1: My Soul >  Bathtub Gin,  555,  Pebbles and Marbles[3]"
-	return: ['My Soul >', 'Bathtub Gin', '555', 'Pebbles and Marbles']
-	"""
-	songslist = []
-	setlist = setlist.get_text().split(r':',1)
-	songslist.append(setlist[0])
-	songs_string = setlist[1]
-	for song in setlist[1].strip().split(r'  '):
-		song = re.sub(r'->|>','',song).strip()
-		song = re.sub(r'\[[0-9]+\]','',song).strip()
-		songslist.append(re.sub(r',$','',song).strip())
-	return songslist, songs_string
+def get_show_ids(year, apikey, verbose=False):
+    """
+    This function will query the phish.net api to get the show ids for 'year'
+      and return a list of all the show ids for that year and write them to a 
+      a text file. 
+    """
+    method = 'pnet.shows.query'
+    params = urllib.urlencode( {
+        'api': apiver,
+        'format': format,
+        'method': method,
+        'apikey': apikey,
+        'year': year
+    })
 
-def get_show_ids(year):
-	"""
-	This function will query the phish.net api to get the show ids for 'year'
-	and return a list of all the show ids for that year.
-	"""
-	method = 'pnet.shows.query'
-	params = urllib.urlencode( {
-		'api': apiver,
-		'format': format,
-		'method': method,
-		'apikey': apikey,
-		'year': year
-	})
+    try:
+        f = urllib.urlopen(url + endpoint + "?%s" % params)
+    except IOError:
+        print(u"Error: Unable to connect. Invalid URL")
+        sys.exit(1)
 
-	try:
-		f = urllib.urlopen(url + endpoint + "?%s" % params)
-	except IOError:
-		print "Error: Unable to connect. Invalid URL"
-		sys.exit(1)
+    response = f.getcode()
+    if response == 200:
+        data = f.read()
+        showlist = json.loads(data)
 
-	response = f.getcode()
-	if response == 200:
-		data = f.read()
-		showlist = json.loads(data)
+        ##--------------------------------------------##
+        ## Check to make sure this year has shows     ##
+        ## if is doesn't call returns a dict with the ##
+        ## reason key saying 'No shows found'         ##
+        ##--------------------------------------------##
+        showids = []
+        if isinstance(showlist, dict):
+            print(u" > {0} in year {1}".format(showlist["reason"], year))
+            return showids
 
-		## check to make sure this year has shows
-		## if is doesn't call returns a dict with the
-		## reason key saying 'No shows found'
-		if isinstance(showlist, dict):
-			print showlist['reason'], "in year:", year
-			return
+        for show in showlist:
+            showid = bs(show['showid']).get_text()
+            showids.append(showid)
 
-		showids = []
-		for show in showlist:
-			showid = bs(show['showid']).get_text()
-			showids.append(showid)
+        if verbose:
+            print(u"   {0} shows found for {1}".format(len(showids), year))
 
-		with open('PhishShowIds/phish_showid_%s.txt' % year,'w') as f:
-			for showid in showids:
-				f.write(year + "," + showid + '\n')
+        return showids
 
-	else:
-		print 'Error - HTTP Response code:', response
+    else:
+        print(u'Error - HTTP Response code: {0}'.format(response))
 
-def get_show(showid, verbose=False):
-	"""
-	This will get the phish show with showid=showid from
-	the phish.net setlist database.
-	"""
-	print "Getting the setlist for showid:"	, showid
+    f.close()
 
-	method='pnet.shows.setlists.get'
-	params = urllib.urlencode( {
-		'api': apiver,
-		'format': format,
-		'method': method,
-		'apikey': apikey,
-		'showid': showid
-	})
+def get_show(showid, apikey, verbose=False):
+    """
+    This will get the phish show with showid=showid from
+    the phish.net setlist database.
+    """
 
-	try:
-		f = urllib.urlopen(url + endpoint + "?%s" % params )
-	except IOError:
-		print "Error: Unable to connect. Invalid URL"
-		sys.exit(1)
+    ##---------------------------------------------------##
+    ## First check to see if show is already in database ##
+    ##---------------------------------------------------##
+    if pshows.find({"showid": showid}).count() != 0:
+        print(u" > Showid: {0} - {1} already in database.".format(showid, pshows.find_one({"showid":showid})["showdate"]))
+        return
 
-	response = f.getcode()
-	if response == 200:
-		data = f.read()
-		show_data = json.loads(data)[0]
+    if verbose:
+        print(u" > Getting the setlist for showid: {0}".format(showid))
 
+    method='pnet.shows.setlists.get'
+    params = urllib.urlencode( {
+        'api': apiver,
+        'format': format,
+        'method': method,
+        'apikey': apikey,
+        'showid': showid
+    })
 
-		showdate = bs(show_data['showdate'])
-		venue = bs(show_data['venue'])
-		city = bs(show_data['city'])
-		state = bs(show_data['state'])
-		if verbose:
-			print "Show:", showdate.get_text()+ " - " +venue.get_text()+ " - " +city.get_text()+ ", " +state.get_text(), "\n"
+    try:
+        f = urllib.urlopen(url + endpoint + "?%s" % params )
+    except IOError:
+        print(u"Error: Unable to connect. Invalid URL")
+        sys.exit(1)
 
-		## first split the text into sets using <p> and class
-		sets = dict()
-		set_string = dict()
-		setlist = bs(show_data['setlistdata'])
-		for pset in setlist.find_all('p', class_=re.compile("pnetset")):
-			songs, sstring = clean_set(pset)
-			sets[songs[0]] = list(songs[1:])
-			set_string[songs[0]] = sstring
+    response = f.getcode()
+    if response == 200:
+        data = f.read()
+        show_data = json.loads(data)[0]
 
-		del show_data['setlistdata']
+        showdate = bs(show_data["showdate"])
+        venue    = bs(show_data["venue"]   )
+        city     = bs(show_data["city"]    )
+        state    = bs(show_data["state"]   )
+        if verbose:
+            print(u"   Show: {0} - {1} - {2}, {3}".format(showdate.get_text(), 
+                                                          venue.get_text()   , 
+                                                          city.get_text()    , 
+                                                          state.get_text()))
 
-		show_data['setlist'] = sets
-		show_data['songs'] = set_string
+        ## Only continue if show date happened before today
+        shows_date  = datetime.datetime.strptime(show_data["showdate"],"%Y-%m-%d").date()
+        if shows_date < todays_date:
 
-		add_show_to_db(show_data)
+            ## First split the text into sets using <p> and class
+            songs_played = set()
+            sets         = dict()
+            sets_string  = dict()
+            setdata = bs(show_data["setlistdata"])
+            for pset in setdata.find_all('p', class_=re.compile("pnetset")):
+                set_name, set_string, set_list = clean_set(pset)
+                if verbose:
+                    print(u"set_name: {0}".format(set_name))
+                    print(u"set_string: {0}".format(set_string))
+                    print(u"set_list: {0}".format(set_list))
+                for song in set_list:
+                    songs_played.add(song)
+                sets[set_name] = set_list
+                sets_string[set_name] = set_string
+    
+            del show_data["setlistdata"]
+            del show_data["relativetime"]
+    
+            show_data["setlist_string"] = sets_string
+            show_data["setlist"] = sets
+            show_data["songs_played"] = list(songs_played)
+    
+            if verbose:
+                pprint.pprint(show_data)
 
-		if verbose:
-			pprint.pprint(show_data)
+            print(u" > Adding show {0} - {1} - {2}, {3} to database.".format(show_data["showdate"],
+                                                                             show_data["venue"]   ,
+                                                                             show_data["city"]    ,
+                                                                             show_data["state"]))    
+            pshows.insert(show_data)
+        else:
+            print(u" > Show on {0} hasn't happened yet.".format(shows_date))
+    
+    else:
+        print(u'Error - HTTP Response code: {0}'.format(response))
+        
+    ## close the connection
+    f.close()
 
-	else:
-		print 'Error - HTTP Response code: ', response
-		
+def clean_set(pn_setlist):
+    """ 
+    This function takes a setlist from phish.net and cleans it up 
+    by splitting it into its songs and keeping segue information.
 
-	## close the connection
-	f.close()
+    ex. 
+    input: "Set 1: My Soul >  Bathtub Gin,  555,  Pebbles and Marbles[3]"
+    return: ['My Soul >', 'Bathtub Gin', '555', 'Pebbles and Marbles']
+    """
+    this_set = pn_setlist.get_text().split(r':',1)
 
-def add_show_to_db(show_info):
-	"""
-	Add the show_info object to the database
-	"""
-	pshows.insert(show_info)
+    set_name = this_set[0]
+    set_string = this_set[1]
+
+    set_list = []
+    for song in set_string.strip().split(r'  '):
+        song = re.sub(r'->|>','',song).strip()
+        song = re.sub(r'\[[0-9]+\]','',song).strip()
+        song = re.sub(r',$','',song).strip()
+        set_list.append(song)
+
+    return set_name, set_string, set_list
+
+def get_api_key(path_to_key):
+    """
+    Get the api key for phish.net from the api_keys.json
+      file located at the path specified from the command
+      line args.
+    """
+    api_key_file = path_to_key + '/api_keys.json'
+    with open(api_key_file) as f:
+        apis = json.load(f)
+        return apis["phishnet_api"]
 
 if __name__ == '__main__':
 
-	for year in range(2009,2015):
-		with open('PhishShowIds/phish_showid_%d.txt'%year,'r') as f:
-			for line in f:
-				showid = line.strip().split(',')[1]
-				get_show(showid)
-				time.sleep(3)
+    parser = argparse.ArgumentParser(prog="phishNet.py")
+    parser.add_argument("-d","--api_dir", help="path to api keys directory"            , required=True              )
+    parser.add_argument("-s","--start"  , help="year from where to start getting shows", default=this_year, type=int)
+    parser.add_argument("-e","--end"    , help="year from where to stop getting shows" , default=this_year, type=int)
+    parser.add_argument("-y","--year"   , help="get shows from only this year"         , default=0        , type=int)
+    args = parser.parse_args()
 
-	#for year in range(1983):
-	#print 'getting showids for', '1983'
-	#get_show_ids(str(1983))
-	#time.sleep(10)
+    ##-------------------------------##
+    ## Get the api key for phish.net ##
+    ##-------------------------------##
+    apikey = get_api_key(args.api_dir)
+
+    ##--------------------------------------------------------##
+    ## Get the showids for the desired year(s) from phish.net ##
+    ##   then add them to the database if not there.          ##
+    ##--------------------------------------------------------##
+    if args.year != 0:
+        args.start = args.end = args.year
+
+    for year in range(args.start, args.end+1):
+        print(u' > Getting showids for {0}'.format(year))
+        show_ids = get_show_ids(str(year), apikey)
+        for id in show_ids:
+            get_show(id, apikey)
+            time.sleep(1)
+        time.sleep(5)
+
+    ##-----------------------------##
+    ## Close connection to MongoDB ##
+    ##-----------------------------##
+    client.close()
+
+
 
 
